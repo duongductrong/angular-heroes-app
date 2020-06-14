9bc019067027fbdb48cc0316aef850b2b8c98aad
Duong Duc Trong <duongductrong06@Gmail.com>
2020-06-14 11:25:16 +0700

    add guard security router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'heroes',
     component: HeroesListComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '**',
