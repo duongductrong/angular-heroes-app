b13b1cc3218feeb39fd227b163ade672b6141346
Duong Duc Trong <duongductrong06@Gmail.com>
2020-06-14 11:32:29 +0700

    add service authenticate

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'src/app/services/message/message.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-messages',
@@ -8,12 +9,20 @@ import { MessageService } from 'src/app/services/message/message.service';
 })
 export class MessagesComponent implements OnInit {
   messages: string[] = [];
+  isAuthenticated: boolean = false;
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
 
   ngDoCheck(): void {
     this.messages = this.messageService.getMessages();
+
+    if (this.isAuthenticated !== this.authService.getAuthenticate()) {
+      this.isAuthenticated = this.authService.getAuthenticate();
+    }
   }
 }
